5f593aed744ec80bed786804a82686a23b62dfc3
jhonnyoddball <jhonny.gonzalez@oddball.io>
2025-10-21 16:16:01 -0400

    Fixed initial routing

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,9 @@ import { NextRequest } from "next/server";
 import { unauthorizedRoutes } from "./app/common/constants/routes";
 import authenticated from "./app/auth/actions/authenticated";
 
-export function middleware(request: NextRequest) {
-  if(!authenticated() && !unauthorizedRoutes.some(route => request.nextUrl.pathname.startsWith(route.path))) {
+export async function middleware(request: NextRequest) {
+  const isAuthenticated = await authenticated();
+  if(!isAuthenticated && !unauthorizedRoutes.some(route => request.nextUrl.pathname.startsWith(route.path))) {
     return Response.redirect(new URL("/auth/login", request.url));
   }
 }
